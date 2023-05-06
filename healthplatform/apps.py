937a2e56abb1a1640f1f4c10b3d5a46c76133c71
Subin <subinsedhai002@gmail.com>
2023-05-07 01:53:10 +0545

    [removed unnecessary preload data]

--- healthplatform/apps.py
+++ healthplatform/apps.py
@@ -9,12 +9,4 @@ class HealthplatformConfig(AppConfig):
 class MyAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'healthplatform'
-# 
-    def ready(self):
-        # Autodiscover all modules in the app to ensure signals are connected
-        autodiscover_modules('signals')
 
-        # Send the startup signal
-        from .signals import startup_signal
-        startup_signal.send(sender=self.__class__)
-# 
