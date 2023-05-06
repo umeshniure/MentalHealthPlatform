5843284280a3a39233be641abaaf14e82fd95975
Sadhanasapkotaa <mandeyra7@gmail.com>
2023-05-06 17:31:17 +0545

    [major update]

--- healthplatform/views.py
+++ healthplatform/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
-# Create your views here.
+# # Create your views here.
+# def test(request):
+#     '''
+#         Description:
+#             This is a function that is used to prevent csrf attack
+        
+#             params:
+#                 request: HttpRequest, required
+#                 id: int, optional
+            
+#             return:
+#                 None if id is not provided. HttpRequest is id is not None
+
+#     '''
