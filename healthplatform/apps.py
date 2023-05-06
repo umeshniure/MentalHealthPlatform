3973d7af409f4ecbda9a419a31ef28e1ec371e8f
Subin <subinsedhai002@gmail.com>
2023-05-07 01:30:57 +0545

    [recommendation system]

--- healthplatform/apps.py
+++ healthplatform/apps.py
@@ -1,6 +1,20 @@
 from django.apps import AppConfig
-
+from django.apps import AppConfig
+from django.utils.module_loading import autodiscover_modules
 
 class HealthplatformConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'healthplatform'
+
+class MyAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'healthplatform'
+# 
+    def ready(self):
+        # Autodiscover all modules in the app to ensure signals are connected
+        autodiscover_modules('signals')
+
+        # Send the startup signal
+        from .signals import startup_signal
+        startup_signal.send(sender=self.__class__)
+# 
